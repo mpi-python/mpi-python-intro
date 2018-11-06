c5b6975204aa7b33840e4bb23d776cdcb9e39f4a
Miguel Angel Santin Schulz <migsan@mpi.nl>
2018-11-06 12:17:15 +0100

    new updates

--- Session3/session3.py
+++ Session3/session3.py
@@ -5,3 +5,16 @@ def fib_list(fib_max):
         fib_nums.append(fib_nums[-1] + fib_nums[-2])
     
     return print(fib_nums)
+
+def delete_dup(my_list):
+    i = 0
+    while i < len(my_list):
+        j = i + 1
+        
+        while j < len(my_list):
+            if my_list[i] == my_list[j]:
+                del my_list[j]
+            else:
+                j += 1
+        i += 1
+    return print(my_list)
