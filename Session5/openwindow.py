4fe95006681cf95b817160645ad9cd3394384f6e
johnhuisman87 <j.l.huisman@let.ru.nl>
2018-11-13 13:49:53 +0100

    Sessio 4 and 5 update

--- Session5/openwindow.py
+++ Session5/openwindow.py
@@ -3,5 +3,5 @@ from psychopy import core, visual
 
 # open a window of 800 by 600 pixels (note the tuple for specifying size!)
 win = visual.Window((800, 600))
-core.wait(5)  # wait for 5 seconds so we can look at the window
+core.wait(10)  # wait for 5 seconds so we can look at the window
 win.close()  # close the window
