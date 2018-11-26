1959575d97d2353d7143059107014270a47534e3
SaskiaAM <s.mooijman@let.ru.nl>
2018-11-26 16:16:08 +0100

    Exercises session 5

--- Session5/hatspin.py
+++ Session5/hatspin.py
@@ -1,5 +1,7 @@
 # import psychopy's core module, and the visual module for presenting visual stimuli
 from psychopy import core, visual, event
+import os 
+os.chdir("./Rotated")
 
 win = visual.Window((800, 600), color = 'orange') # open a window of 800 by 600 pixels 
 core.wait(3) #show window for n seconds
@@ -12,6 +14,18 @@ count = 1
 counter = 1
 
 while True:
+    keys = event.getKeys(keyList=['space', 'escape', 'up', 'down', 'right', 'left'])
+    
+    if 'escape' in keys:
+        break
+        
+    elif 'space' in keys:
+        event.waitKeys(keyList=['space'])
+        
+    elif 'up' in keys:
+        counter += 1
+                              
+    
     image.setImage(f'Hat{count}.png')
     image.draw()
     win.flip()
@@ -22,6 +36,5 @@ while True:
         count = 1
     if count < 1:
         count = 360
-    
     
 win.close()  # close the window
